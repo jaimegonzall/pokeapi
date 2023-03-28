10b9e7371a22bdae09df252d688559128ca59f01
EXEVI\jjgonzalez <jaime.gonzalez@sngular.com>
2023-03-29 01:57:05 +0200

    PKA-101 detail view layout

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,9 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
@@ -19,13 +17,11 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style scoped lang="scss">
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+  // line-height: 1.5;
+  // max-height: 100vh;
+  display: flex;
+  place-items: center;
+  padding-right: calc(var(--section-gap) / 2);
 }
 
 nav {
@@ -53,30 +49,4 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -41,29 +41,112 @@ const gettingPokemonLoading = computed(() => {
     v-else
     class="detail-view"
   >
-    <h1>Detail View</h1>
-    <h1>{{ capitalize(`${gettingPokemon.name}`) }}</h1>
-    <h6>Medidas:</h6>
-    <p>Altura: {{ gettingPokemon.height }}</p>
-    <p>Peso: {{ gettingPokemon.weight }}</p>
-    <h6>Tipos:</h6>
-    <ul v-for="(type, index) in gettingPokemon.types" :key="index">
-      <li>{{type.type.name}}</li>
-    </ul>
-    <h6>Capacidades:</h6>
-    <ul v-for="(stat, index) in gettingPokemon.stats" :key="index">
-      <li>{{stat.stat.name}}: {{stat.base_stat}}</li>
-    </ul>
-    <!-- <pre>{{ gettingPokemon }}</pre> -->
+    <div class="detail-view__data">
+      <div class="detail-view__data-header">
+        <h2>
+          <span class="detail-view__data-header__id">#{{ gettingPokemon.id }}</span> {{ capitalize(`${gettingPokemon.name}`) }}
+        </h2>
+        <ul class="pokemon-type-list">
+          <li
+            v-for="(type, index) in gettingPokemon.types"
+            :key="index"
+            :class="['pokemon-type-list__item', `type-${type.type.name}`]"
+          >
+            {{type.type.name}}
+          </li>
+        </ul>
+      </div>
+      <div class="detail-view__body">
+        <p>Altura: {{ gettingPokemon.height }}</p>
+        <p>Peso: {{ gettingPokemon.weight }}</p>
+        <ul class="pokemon-stat-list">
+          <li
+            v-for="(stat, index) in gettingPokemon.stats" :key="index"
+            class="pokemon-stat-list__item"
+          >
+            <div class="pokemon-stat-list__area">{{stat.stat.name}}</div>
+            <div class="pokemon-stat-list__area">{{stat.base_stat}}</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="detail-view__image">
+      <img
+        :src="gettingPokemon.sprites?.other.home.front_default"
+        alt="gettingPokemon.name"
+      >
+    </div>
+    <!-- <pre>{{gettingPokemon}}</pre> -->
   </div>
 </main>
 </template>
 
 <style scoped lang="scss">
 
+@import '../assets/sass/settings/color-tokens';
+@import '../assets/sass/settings/fonts';
+
 .detail-view {
-    flex-basis: 1px;
-    flex-grow: 1;
+  flex-basis: 1px;
+  flex-grow: 1;
+  height: fit-content;
+  max-width: 800px;
+  padding: 16px;
+  border-radius: 16px;
+  box-shadow: 0 6px 30px -6px rgba(0, 0, 0, 0.25);
+  display: grid;
+  grid-template-columns: 50% 50%;
+  &__data {
+    &-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      &__id {
+        color: $grey-30;
+        font-size: $font-size-base;
+      }
+    }
+  }
+  &__image {
+    text-align: center;
+    padding: 16px;
+    & img {
+      max-width: 100%;
+      max-height: 100%;
+    }
+  }
+}
+
+.pokemon-type-list {
+  list-style: none;
+  display: flex;
+  gap: 8px;
+  &__item {
+    display: inline-block;
+    padding: 0px 8px;
+    border-radius: 4px;
+  }
+}
+
+.pokemon-stat-list {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding-left: 0;
+  list-style: none;
+  &__item {
+    display: grid;
+    grid-template-columns: 150px 1fr;
+    gap: 4px;
+    border-radius: 4px;
+  }
+  &__area {
+    padding: 4px 8px;
+    background-color: $grey-10;
+    &:nth-child(2) {
+      text-align: right;
+    }
+  }
 }
 
 </style>
